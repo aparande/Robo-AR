ba4b70d000c16015731a87c2660b68d2964be1d2
Anmol Parande <parandea1.7@gmail.com>
2020-11-20 10:35:03 -0800

    Broken notify characteristic

--- Robo-AR-IOS/Robo-AR/BLEViewController.swift
+++ Robo-AR-IOS/Robo-AR/BLEViewController.swift
@@ -12,13 +12,17 @@ import CoreBluetooth
 class BLEViewController: UIViewController {
     var instructions: [Instruction] = []
     var centralManager: CBCentralManager!
-    var romiPeripheral: CBPeripheral!
+    var romiPeripheral: CBPeripheral?
     
-    var instructionCharacteristic: CBCharacteristic!
+    var instructionCharacteristic: CBCharacteristic?
+    var acknowledgeCharacteristic: CBCharacteristic?
     
-    let ROMI_NAME = "DNEG"
-    let ROMI_SERVICE_UUID = CBUUID(string: "4607EDA0-F65E-4D59-A9FF-84420D87A4CA")
-    let ROMI_INSTRUCTION_CHARACTERISTIC_UUID = CBUUID(string: "4607EDA1-F65E-4D59-A9FF-84420D87A4CA")
+    static let ROMI_NAME = "Robo-AR"
+    static let ROMI_SERVICE_UUID = CBUUID(string: "4607EDA0-F65E-4D59-A9FF-84420D87A4CA")
+    static let ROMI_INSTRUCTION_CHARACTERISTIC_UUID = CBUUID(string: "4607EDA1-F65E-4D59-A9FF-84420D87A4CA")
+    static let ROMI_ACKNOWLEDGE_CHARACTERISTIC_UUID = CBUUID(string: "4607EDA2-F65E-4D59-A9FF-84420D87A4CA")
+    
+    static let ROMI_CHARACTERISTIC_UUIDS = [ROMI_INSTRUCTION_CHARACTERISTIC_UUID, ROMI_ACKNOWLEDGE_CHARACTERISTIC_UUID]
     
     var lastExecutedInstruction = -1
     
@@ -32,14 +36,19 @@ class BLEViewController: UIViewController {
             return
         }
         
+        guard let romi = romiPeripheral, let characteristic = instructionCharacteristic else {
+            print("Wasn't connected to Romi")
+            return
+        }
+        
         var instruction = instructions[lastExecutedInstruction + 1]
         var payload = Data(buffer: UnsafeBufferPointer(start: &instruction.distance, count: 1))
         payload.append(Data(buffer: UnsafeBufferPointer(start: &instruction.angle, count: 1)))
         
-        romiPeripheral.writeValue(payload, for: instructionCharacteristic, type: CBCharacteristicWriteType.withResponse)
+        romi.writeValue(payload, for: characteristic, type: CBCharacteristicWriteType.withResponse)
         
         lastExecutedInstruction += 1
-        print("Transmitted instruction \(lastExecutedInstruction): (\(instructions[lastExecutedInstruction]), ")
+        print("Transmitted instruction \(lastExecutedInstruction): (\(instructions[lastExecutedInstruction])")
     }
     
     func discoveredInstructionCharacteristic() {
@@ -67,18 +76,18 @@ extension BLEViewController: CBCentralManagerDelegate {
     }
     
     func centralManager(_ central: CBCentralManager, didDiscover peripheral: CBPeripheral, advertisementData: [String : Any], rssi RSSI: NSNumber) {
-        if peripheral.name == ROMI_NAME {
+        if peripheral.name == BLEViewController.ROMI_NAME {
             print(peripheral)
             romiPeripheral = peripheral
-            romiPeripheral.delegate = self
+            romiPeripheral?.delegate = self
             centralManager.stopScan()
-            centralManager.connect(romiPeripheral, options: nil)
+            centralManager.connect(peripheral, options: nil)
         }
     }
     
     func centralManager(_ central: CBCentralManager, didConnect peripheral: CBPeripheral) {
         print("Connected to Romi")
-        romiPeripheral.discoverServices([ROMI_SERVICE_UUID])
+        romiPeripheral?.discoverServices([BLEViewController.ROMI_SERVICE_UUID])
     }
 }
 
@@ -88,29 +97,45 @@ extension BLEViewController: CBPeripheralDelegate {
         for service in services {
             print(service)
             
-            peripheral.discoverCharacteristics([ROMI_INSTRUCTION_CHARACTERISTIC_UUID], for: service)
+            peripheral.discoverCharacteristics(BLEViewController.ROMI_CHARACTERISTIC_UUIDS, for: service)
         }
     }
     
     func peripheral(_ peripheral: CBPeripheral, didDiscoverCharacteristicsFor service: CBService, error: Error?) {
         guard let characteristics = service.characteristics else { return }
         for characteristic in characteristics {
-            if characteristic.uuid == ROMI_INSTRUCTION_CHARACTERISTIC_UUID {
+            if characteristic.uuid == BLEViewController.ROMI_INSTRUCTION_CHARACTERISTIC_UUID {
                 // Kick off the instruction sending process
                 print("Found instruction characteristic")
                 instructionCharacteristic = characteristic
                 discoveredInstructionCharacteristic()
+            } else if characteristic.uuid == BLEViewController.ROMI_ACKNOWLEDGE_CHARACTERISTIC_UUID {
+                print("Found acknowledge characteristic")
+                acknowledgeCharacteristic = characteristic
+                romiPeripheral?.setNotifyValue(true, for: characteristic)
+                print(acknowledgeCharacteristic?.properties.contains(.notify))
+                romiPeripheral?.discoverDescriptors(for: characteristic)
             }
         }
     }
     
     func peripheral(_ peripheral: CBPeripheral, didUpdateValueFor characteristic: CBCharacteristic, error: Error?) {
         switch characteristic.uuid {
-        case ROMI_INSTRUCTION_CHARACTERISTIC_UUID:
-            print(characteristic.value ?? "No Value")
+        case BLEViewController.ROMI_INSTRUCTION_CHARACTERISTIC_UUID:
+            print(characteristic.value ?? "No value for instruction")
+        case BLEViewController.ROMI_ACKNOWLEDGE_CHARACTERISTIC_UUID:
+            print(characteristic.value ?? "No value for acknowledge")
         default:
             print("Unhandled characteristic UUID: \(characteristic.uuid)")
         }
+    }
+    
+    func peripheral(_ peripheral: CBPeripheral, didUpdateNotificationStateFor characteristic: CBCharacteristic, error: Error?) {
+        print("Updated characteristic notification \(characteristic) \(error)")
+    }
+    
+    func peripheral(_ peripheral: CBPeripheral, didDiscoverDescriptorsFor characteristic: CBCharacteristic, error: Error?) {
+        print(characteristic.descriptors)
     }
 }
 
